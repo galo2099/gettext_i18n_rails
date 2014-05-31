eed81cad7f6166236c2d0f3f0027da621ff90917
galo2099 <robsonbraga@gmail.com>
2014-05-31 15:08:46 -0700

    Do not replace _ with -.

--- lib/gettext_i18n_rails/i18n_hacks.rb
+++ lib/gettext_i18n_rails/i18n_hacks.rb
@@ -3,7 +3,7 @@ I18n::Config # autoload
 module I18n
   class Config
     def locale
-      FastGettext.locale.gsub("_","-").to_sym
+      FastGettext.locale.to_sym
     end
 
      def locale=(new_locale)
